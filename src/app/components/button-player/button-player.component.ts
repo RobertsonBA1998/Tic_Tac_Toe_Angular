a7749c1e6e4296ae6db3eac456fac0da55e3ebb7
RobertsonBA <113301827+RobertsonBA1998@users.noreply.github.com>
2023-08-26 22:35:45 -0400

    updated buton/board component, updated logic

--- src/app/components/button-player/button-player.component.ts
+++ src/app/components/button-player/button-player.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button-player',
   templateUrl: './button-player.component.html',
-  styleUrls: ['./button-player.component.css']
+  styleUrls: ['./button-player.component.css'],
 })
 export class ButtonPlayerComponent {
+  //passing value from parent to children to determine X and O
+  @Input() value: string = '';
 
+  //passing value from parent to children to determine disabled property true/false from btn
+  @Input() disabled: boolean = false;
+
+  //creating custom event to let parent ONLY KNOW that an event has occured
+  @Output() btnClicked = new EventEmitter<void>();
+
+  //disable button if clicked after event
+  handleClick() {
+    if (!this.disabled) {
+      this.btnClicked.emit();
+    }
+  }
 }
--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-board.component.css'],
 })
 export class GameBoardComponent {
-  boardCells: string[] = Array.from({ length: 9 }, () => '');
+  //player first starts off as X (which is player 1. O is player 2)
+  currentPlayer: string = 'X';
+
+  //showcases players turn and gamestate after all index filled
+  playersStatus: string = '';
+
+  //used to disable buttons when the game is finished
+  gameDone: boolean = false;
+
+  //create an array of nine boardCells to track data of players and gamestate
+  boardCells: string[] = new Array(9).fill('');
+
+  //players occupies these spaces in their index, will be declared winners
+  winningConditions: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  //showcases the status of the turns of X and O
+  playersStatusFunct() {
+    if (this.currentPlayer === 'X') {
+      this.playersStatus = "Player 1's Turn";
+    } else if (this.currentPlayer === 'O') {
+      this.playersStatus = "Player 2's Turn";
+    }
+  }
+
+  //determines the game's winners
+  gamesWinner() {
+    //if the winnning for X and O conditions arent met, it is a tie
+    let noWinner = false;
+
+    // Loop through all winning condition variable
+    for (let i = 0; i < this.winningConditions.length; i++) {
+      const condition = this.winningConditions[i];
+
+      //verify if the winning conditions are met for X and O
+      const winnerX = condition.every(
+        (index) => this.boardCells[index] === 'X'
+      );
+      const winnerO = condition.every(
+        (index) => this.boardCells[index] === 'O'
+      );
+
+      //if the condiitons are met display winner and siable buttons
+      if (winnerX) {
+        this.playersStatus = 'X has won!';
+        this.gameDone = true;
+      } else if (winnerO) {
+        this.playersStatus = 'O has won!';
+        this.gameDone = true;
+      }
+    }
+
+    //checks if all cells are filled, in order to verify if there is no winner from X and o
+    //sets it to true, and disables tie text, and disables buttons
+    if (!noWinner && this.boardCells.every((cell) => cell !== '')) {
+      this.playersStatus = "It's a tie!";
+      this.gameDone = true;
+    }
+  }
+
+  //function to determine where X and O go in their own index
+  makeMove(index: number) {
+    if (!this.gameDone && this.boardCells[index] === '') {
+      //fill in array with current player
+      this.boardCells[index] = this.currentPlayer;
+
+      // determines when is X and O in the board
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+    }
+
+    //run these functions to update/determine winner and player status
+    this.playersStatusFunct();
+    this.gamesWinner();
+  }
 }
